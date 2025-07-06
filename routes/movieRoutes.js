3ba3118282d12d4c62de0ef355a35c948430ddf8
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-07-06 17:52:04 +0200

    refactor: update request examples and improve formatting in movie routes

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express'),
-router = express.Router();
+  router = express.Router();
 
 const knex = require('../db');
 
@@ -7,9 +7,10 @@ router.get('/', async (req, res) => {
   const movies = await knex('movies').select('*');
   res.json(movies);
 });
+
 router.get('/:id', (req, res) => {
   const movieId = req.params.id;
- knex('movies').where('id', movieId).first().then((movie) => {
+  knex('movies').where('id', movieId).first().then((movie) => {
     if (movie) {
       res.json(movie);
     } else {
@@ -19,8 +20,20 @@ router.get('/:id', (req, res) => {
     console.error(err);
     res.status(500).json({ error: 'Internal server error' });
   });
-
-
 });
+
+router.post('/search', (req, res) => {
+  const searchTerm = req.body;
+knex('movies')
+  .where('title', 'like', `%${searchTerm.title}%`)
+  .where( 'year', 'like', `${searchTerm.year}%`)
+    .then((movies) => {
+      res.status(200).json(movies);
+    }).catch((err) => {
+      console.error(err);
+
+    });
+});
+
 
 module.exports = router;
