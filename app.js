519fe1416c5578ac98a5548ae724b263e9c20103
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-07-06 20:50:31 +0200

    refactor: add dotenv configuration and update package dependencies

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
 const movieRoutes = require('./routes/movieRoutes');
-app.use(express.json());
+require('dotenv').config();
 
+app.use(express.json());
 app.use('/movies', movieRoutes);
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
