008f03733e5f5f01e2ccbc87822c97742aa9aa5c
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-07-09 14:15:24 +0200

    refactor: update default poster URL in movies table migration

--- migrations/20250706111120_create_movies_table.js
+++ migrations/20250706111120_create_movies_table.js
@@ -10,7 +10,7 @@ exports.up = function(knex) {
     table.float('rating').notNullable();
     table.string('director').notNullable().defaultTo('Unknown');
     table.text('description').notNullable();
-    table.string('poster_url').notNullable().defaultTo('https://via.placeholder.com/300');
+    table.string('poster_url').notNullable().defaultTo('https://picsum.photos/200/300');
     table.timestamps(true, true); // created_at and updated_at timestamps
   });
 };
@@ -24,13 +24,3 @@ exports.down = function(knex) {
 };
 
 
-/*
-
-írj olyan mysql lekérdezést ami 5 táblából veszi az adatot. Minden adat csak egyszer szerepeljen. 
-A táblák:
- movies - oszlopai: id, title, year, rating, director, description, poster_url
- genres - oszlopai: id, name, description, created_at, updated_at
- movie-genre - oszlopai: id, movie_id, genre_id, created_at, updated_at
-movie-actors - oszlopai: id, movie_id, actor_id 
-actors - oszlopai: id, fullName, bio, sex, date_of_birth
-*/
