02a9d625adfc183469007a335c496f4aafd496bf
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-07-10 12:09:39 +0200

    refactor: enhance error handling and streamline movie retrieval functions

--- services/movieService.js
+++ services/movieService.js
@@ -3,30 +3,36 @@ import knex from '../db.js';
 const getAllMovies=async (req,res)=>{
     const query = "SELECT   m.id AS movie_id,  m.title,  m.year,  m.rating,  m.director,  m.description,  m.poster_url,  GROUP_CONCAT(DISTINCT g.name ORDER BY g.name SEPARATOR ', ') AS genres,  GROUP_CONCAT(DISTINCT a.fullName ORDER BY a.fullName SEPARATOR ', ') AS actors FROM movies AS m LEFT JOIN `movie-genre` AS mg ON m.id = mg.movie_id LEFT JOIN   genres AS g ON mg.genre_id = g.id LEFT JOIN `movie-actors` AS ma ON m.id = ma.movie_id  LEFT JOIN  actors AS a ON ma.actor_id = a.id GROUP BY m.id, m.title, m.year, m.rating, m.director, m.description, m.poster_url;"
     return await knex.raw(query).then((alldata) => {
-    const formattedData = formatResultObject(alldata[0])
-    res.json(formattedData)
-  }).catch((err) => {
-    res.status(500).json({ error: 'Internal server error' });
-  })
-}
-const getAllGenre=async ()=>{
+      const formattedData = formatResultObject(alldata[0])
+      res.json(formattedData)
+    }).catch((err) => {
+      res.status(500).json({ error: 'Internal server error' });
+    })
+  }
+  const getAllGenre=async ()=>{
     return await knex('genres')
     .select('*')
-}
-const getMovieById=async (id)=>{
-    return await knex('movies')
-    .where('id', id)
-    .first()
-}
-const getMoviesByGenre=async (id)=>{
+  }
+  const getMovieById=async (id)=>{
+    const query = "SELECT   m.id AS movie_id,   m.title,  m.year,  m.rating,  m.director,  m.description,  m.poster_url,  GROUP_CONCAT(DISTINCT g.name ORDER BY g.name SEPARATOR ', ') AS genres,  GROUP_CONCAT(DISTINCT a.fullName ORDER BY a.fullName SEPARATOR ', ') AS actors FROM   movies AS m LEFT JOIN `movie-genre` AS mg ON m.id = mg.movie_id LEFT JOIN genres AS g ON mg.genre_id = g.id LEFT JOIN `movie-actors` AS ma ON m.id = ma.movie_id LEFT JOIN actors AS a ON ma.actor_id = a.id WHERE m.id = ? GROUP BY  m.id, m.title, m.year, m.rating, m.director, m.description, m.poster_url;"
+    return await knex.raw(query,[id]).then((data) => {
+      const formattedData = formatResultObject(data[0])
+      return formattedData[0]
+    }).catch((err) => {
+      console.error(err);
+    });
+  }
+  const getMoviesByGenre=async (id)=>{
     return await knex('movie-genre')
     .where('genre_id', id)
     .select('movie_id')
-}
-const getMoviesById=async (idArray)=>{
+  }
+  const getMoviesById=async (idArray)=>{
     return await knex('movies')
     .whereIn('id', idArray)
-    .select('*')
+    .select('*').then(movie =>{
+      return movie
+    })
 }
 const formatResultObject =(data)=>{
    data.forEach(data => {
